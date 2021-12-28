c475fd58547ca526d83d3373e83af98b2ed9849c
Nam Pham <nampdn@protonmail.com>
2021-12-28 01:31:04 +0000

    chore(config): update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,12 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-jodie/gatsby-config.js
-    siteTitle: `Jodie`,
-    siteTitleAlt: `Jodie - Gatsby Starter Portfolio`,
-    siteHeadline: `Jodie - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://jodie.lekoarts.de`,
-    siteDescription: `Image-heavy photography portfolio with colorful accents & customizable pages. Includes adaptive image grids powered by CSS grid and automatic image integration into projects.`,
-    siteLanguage: `en`,
+    siteTitle: `Nam & Linh Family`,
+    siteTitleAlt: `Nam & Linh`,
+    siteHeadline: `Nam & Linh`,
+    siteUrl: `https://namlinh.family`,
+    siteDescription: `Nam & Linh Family`,
+    siteLanguage: `vi`,
     siteImage: `/banner.jpg`,
     author: `@lekoarts_de`,
   },
